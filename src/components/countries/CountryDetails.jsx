62a9b008288c79cfa8a08cc5e7d31b1fdaabd6a3
Adrian Rangel <adrijar21@gmail.com>
2022-07-06 23:31:53 -0400

    added styles to details

--- src/components/countries/CountryDetails.jsx
+++ src/components/countries/CountryDetails.jsx
@@ -1,7 +1,9 @@
-import { Heading, Text, Button, Flex, Image, Grid, GridItem, Spinner } from '@chakra-ui/react';
+import { Heading, Text, Button, Flex, Image, Grid, GridItem, Spinner, Box, Icon } from '@chakra-ui/react';
+import { FaArrowLeft } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 import { useState, useEffect } from 'react';   
 
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 import { getCountry } from '../../services/GetCountries';
 
@@ -15,11 +17,43 @@ export const CountryDetails = () => {
   
   return  (
     <>
-      {country.map((country) => (
-        <Heading>{country.name.common}</Heading>
-      ))}
+      <Link to="/">
+        <Button as={motion.button} m="4rem" w="120px" boxShadow="lg" whileHover={ {y:"5px", scale:1.1} } transition=".1s ease">
+          <Flex alignItems="center" fontWeight="normal">
+            <Icon as={FaArrowLeft} mr=".5rem" />
+            <Text>Back</Text>
+          </Flex>
+        </Button>
+      </Link>
+      
+      {country.length == 0 ? (<Box position="relative" w="100%" h="100vh">
+        <Spinner size="lg" color='blue' position="absolute" top="100%" left="50%" transform="translate(-50%, -50%)" />
+      </Box>) : (
+        <Box w="100%" p="1rem">
+          {country.map((country) => (
+            <Flex justifyContent="space-evenly">
+              <Image src={country.flags.svg} w="380px" />
+              <Box>
+                <Heading mb="1.5rem">{country.name.common}</Heading>
+                <Grid templateColumns="repeat(2, 1fr)" gap="5px" textAlign="left" w="100%">
+                  <GridItem>
+                    <Text><strong>Native Name</strong>: {}</Text>
+                    <Text><strong>Population</strong>: {country.population}</Text>
+                    <Text><strong>Region</strong>: {country.region}</Text>
+                    <Text><strong>Sub Region</strong>: {country.subregion}</Text>
+                    <Text><strong>Capital</strong>: {country.capital}</Text>
+                  </GridItem>
+                  <GridItem>
+                    <Text><strong>Top Level Domain</strong>: {country.tld}</Text>
+                    <Text><strong>Currencies</strong>: {}</Text>
+                    <Text><strong>Languages</strong>: {}</Text>
+                  </GridItem>
+                </Grid>
+              </Box>
+            </Flex>
+          ))}
+        </Box>
+        )}
     </>
   )
-
-
 }
